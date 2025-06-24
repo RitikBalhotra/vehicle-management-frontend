9381caa4a4c6897484384c6a076c12c0f4c25feb
Ritik Balhotra <121785590+RitikBalhotra@users.noreply.github.com>
2025-06-24 12:54:36 +0530

    add static backend url for testing

--- src/Service/APIService.tsx
+++ src/Service/APIService.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import StorageService from '../Service/StorageService';
 
 // Make sure to define REACT_APP_API_URL in your .env file (e.g., REACT_APP_API_URL=http://localhost:5000)
-const baseUrl = import.meta.env.VITE_API_URL || '';
+const baseUrl = "https://vehicle-management-server-1-ly86.onrender.com/api"
 
 
 interface ApiParams {
