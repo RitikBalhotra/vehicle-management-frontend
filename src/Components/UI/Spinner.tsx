9c20c4f6a9ad49ed07626421ab3124277c2e04f9
Ritik Balhotra <121785590+RitikBalhotra@users.noreply.github.com>
2025-06-24 15:33:18 +0530

    minor

--- src/Components/UI/Spinner.tsx
+++ src/Components/UI/Spinner.tsx
@@ -24,7 +24,7 @@ const Spinner: React.FC = () => {
                     alignItems: 'center',
                     backdropFilter: 'blur(1px)',
                     backgroundColor: 'rgba(0, 0, 255, 0.3)', // semi-transparent blue
-                    zIndex: 2500,
+                    zIndex: 20000,
                     pointerEvents: 'all',
                     overflow: 'hidden',
                 }}
