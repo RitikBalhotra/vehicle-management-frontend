6cced2dafe01eccb6e284c8c791d09a360358ac2
Ritik Balhotra <121785590+RitikBalhotra@users.noreply.github.com>
2025-06-24 22:00:01 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // import { Provider } from 'react-redux';
 // import { store } from './app/store';
-import Spinner from './Components/UI/Spinner';
 import { BrowserRouter } from 'react-router-dom';
 import AppToast from './Components/UI/AppToast';
+import Spinner from './Components/UI/Spinner';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
     <AppToast />
     <BrowserRouter>
-    <Spinner />
+      <Spinner />
       <App />
     </BrowserRouter>
     {/* </Provider> */}
